6e42de86181cc05d86a74e07af85be5ee0e6008c
Ezekiel <iamwebwiz@gmail.com>
2022-09-26 23:15:12 +0100

    update stubs & db configuration

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,22 +1,22 @@
 export const config = {
-  "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
-    "dialect": "postgres",
-    "aws_region": "us-east-2",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+  dev: {
+    username: "postgres",
+    password: "postgres",
+    database: "udagram_restapi",
+    host: "localhost",
+    dialect: "postgres",
+    aws_region: "us-east-1",
+    aws_profile: "default",
+    aws_media_bucket: "udagram-ezekiel-dev",
   },
-  "jwt": {
-    "secret": " "
+  jwt: {
+    secret: "helloworld",
   },
-  "prod": {
-    "username": "",
-    "password": "",
-    "database": "udagram_prod",
-    "host": "",
-    "dialect": "postgres"
-  }
-}
+  prod: {
+    username: "",
+    password: "",
+    database: "udagram_prod",
+    host: "",
+    dialect: "postgres",
+  },
+};
